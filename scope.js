6a2ff8cc4d44e081cbf189722bfaae20b3f7fe93
Prashant1659 <singhkaushal9561@gmail.com>
2025-01-24 23:39:57 +0530

    scopes in js

---
+++ scope.js
@@ -0,0 +1,15 @@
+
+//Scopes in node environment and scope in browser js console is different.
+
+//In this case the functoin will execute and we will have an output.
+console.log(addOne(5));
+function addOne(num){
+    return num + 1;
+}
+
+console.log(addTwo(4));//This will not execute as function is defined after this call (Hoisting-{Refer mdn for more}).
+
+//addTwo is a expression now as it holds some value
+const addTwo = function(num){ // This is known as hoisting 
+    return num + 2;
+}
