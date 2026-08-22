07e86a4cbd837e8fceaadfc7d8958c2346a7c9fd
Prashant <singhkaushal9561@gmail.com>
2026-08-22 15:04:00 +0530

    function expression vs function

--- scope.js
+++ scope.js
@@ -13,3 +13,5 @@ console.log(addTwo(4));//This will not execute as function is defined after this
 const addTwo = function(num){ // This is known as hoisting 
     return num + 2;
 }
+//the above function executed because it is not assigned to a variable whereas the addTwo function is assigned to a variable(function expression),
+// in JS we need to declare a variable before using it, exception "var" to know more about function expression [`https://www.w3schools.com/js/js_function_expressions.asp`]
